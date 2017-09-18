4c2376ca1ad7dba2fa5fdf67c2a96081b50facbf
Matshwenyego_II <Ashleyngoetjana@gmail.com>
2017-09-18 08:15:30 +0200

    dev and prod keys added to googleoauth2

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "689200109135-ssuscggdp3quceqjk9k4p2l5nscni0k6.apps.googleusercontent.com",
+  googleClientSecret: "93kBP9064ZbqBrMfntLRVvbQ",
+  // mongoURI: "mongodb://admin:admin@ds141284.mlab.com:41284/recruitsdb",
+  mongoURI: "mongodb://localhost:27017/recruitdb",
+  cookieKeys: "Dej@vu242158671980ogjr80@gmail.comDejavu2421586"
+};
